533fbdeb809bcef0fcd240e8469d5962e9b3ac35
KashanAdnan <khanzaidaboy@gmai.com>
2022-11-17 01:21:13 -0800

    monogse-problem

--- public/Sign Up/script.js
+++ public/Sign Up/script.js
@@ -12,7 +12,7 @@ const sinup = () => {
         confPassword: document.getElementById("confirm-password").value,
     }
     const url = "http://localhost:3000"
-    const Http = new XMLHttpRequest;
+    const Http = new XMLHttpRequest();
 
     Http.open("POST", port + "/signup");
     Http.setRequestHeader("Content-Type", "application/json");
@@ -24,43 +24,43 @@ const sinup = () => {
             let jsonRes = JSON.parse(Http.responseText);
             if (Http.status === 200) {
                 alert(jsonRes.message)
-                window.location.reload()
-                location.replace("http://localhost:3000/home.html");
-                var loginemail = document.getElementById("email");
-                var loginuser = document.getElementById("username");
-                var loginphone = document.getElementById("phone");
-                var loginpasss = document.getElementById("password");
-                var loginconpass = document.getElementById("confirm-password");
+                // window.location.reload()
+                // location.replace("http://localhost:3000/in.html");
+                // var loginemail = document.getElementById("email");
+                // var loginuser = document.getElementById("username");
+                // var loginphone = document.getElementById("phone");
+                // var loginpasss = document.getElementById("password");
+                // var loginconpass = document.getElementById("confirm-password");
 
-                if (loginuser.value === ' ') {
-                    alert("***Please Fill User   Input !")
-                }
-                else if (loginemail.value === ' ') {
-                    alert("***Please Fill Email Input !")
-                }
-                else if (loginpasss.value === ' ') {
-                    alert("***Please Fill Password Input !")
-                }
-                else if (loginconpass.value === ' ') {
-                    alert("***Please Fill Password Input !")
-                }
-                else if (loginpasss.value < 8 && loginconpass.value < 8) {
-                    alert("***The Password Must Be 8 Character !")
-                }
+                // if (loginuser.value === ' ') {
+                //     alert("***Please Fill User   Input !")
+                // }
+                // else if (loginemail.value === ' ') {
+                //     alert("***Please Fill Email Input !")
+                // }
+                // else if (loginpasss.value === ' ') {
+                //     alert("***Please Fill Password Input !")
+                // }
+                // else if (loginconpass.value === ' ') {
+                //     alert("***Please Fill Password Input !")
+                // }
+                // else if (loginpasss.value < 8 && loginconpass.value < 8) {
+                //     alert("***The Password Must Be 8 Character !")
+                // }
 
-                else if (loginphone.value === ' ') {
-                    alert("***Please Fill Phone Input !")
-                }
-                else if (loginconpass.value !== loginpasss.value) {
-                    alert("***Please Match the Password & Confirm Password  Input !")
-                }
+                // else if (loginphone.value === ' ') {
+                //     alert("***Please Fill Phone Input !")
+                // }
+                // else if (loginconpass.value !== loginpasss.value) {
+                //     alert("***Please Match the Password & Confirm Password  Input !")
+                // }
 
 
-                localStorage.setItem('name', loginuser.value)
+                // localStorage.setItem('name', loginuser.value)
             }
             else {
                 alert(jsonRes.message)
-                window.location.reload()
+                // window.location.reload()
             }
         }
     }
