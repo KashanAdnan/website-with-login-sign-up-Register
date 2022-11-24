8d01258c5f0073318db5f805ac6be5d0ad881858
KashanAdnan <khanzaidaboy@gmai.com>
2022-11-24 09:29:39 -0800

    some-code

--- server.js
+++ server.js
@@ -87,12 +87,12 @@ app.post("/login", (req, res, next) => {
           res.status(200).send({
             message: "Successfully login  !",
           });
-          if (data === adminObj) {
-            res.status(200).send({
-              message: "Going To Admin Page Please Wait .............",
-            });
-          } 
         }
+       else if (data.email === adminObj.email && data.password === adminObj.password) {
+          res.status(200).send({
+            message: "Going To Admin Page Please Wait .............",
+          });
+        } 
         else {
           res.status(405).send({
             message: "User Not Exits Please Sign Up !",
