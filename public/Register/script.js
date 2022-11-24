6af9336ec2b85f6a794e8e17a631346b06abec03
KashanAdnan <khanzaidaboy@gmai.com>
2022-11-24 11:37:25 -0800

    some-code

--- public/Register/script.js
+++ public/Register/script.js
@@ -1,23 +1,23 @@
 function admit() {
-    var addobj = {
+    var obj = {
         stDname : document.getElementById("username").value,
         email : document.getElementById("email").value,
         age : document.getElementById("age").value,
         contactno : document.getElementById("phone").value,
         nationality : document.getElementById("nation").value,
-        brithDate : document.getElementById("Birthdate").value,
         placeofbirth : document.getElementById("placeofbirth").value,
         level : document.getElementById("level").value
     }
-    var Http = new XMLHttpRequest;
-    Http.open("POST",   "https://sir-web.herokuapp.com" + "/admission");
+    var Http = new XMLHttpRequest();
+    var port =  "https://sir-web.herokuapp.com" ;
+    Http.open("POST", "https://sir-web.herokuapp.com" +"/admission");
     Http.setRequestHeader("Content-Type", "application/json");
-    Http.send(JSON.stringify(addobj));
+    Http.send(JSON.stringify(obj));
     Http.onreadystatechange = (e) => {
         console.log(e)
         if (Http.readyState === 4) {
             console.log(Http.status)
-            let jsonRes = JSON.parse(Http.responseText);
+            var jsonRes = JSON.parse(Http.responseText);
             if (Http.status === 200) {
                 swal("Good job!", jsonRes.message, "success");
             } else {
