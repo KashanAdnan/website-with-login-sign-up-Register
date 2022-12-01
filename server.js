5bad8b2c804198907219943079ff85173b72db25
KashanAdnan <khanzaidaboy@gmai.com>
2022-11-30 21:37:58 -0800

    Some-code

--- server.js
+++ server.js
@@ -84,6 +84,7 @@ app.post("/login", (req, res, next) => {
           res.status(405).send({
             message: "User Not Exits Please Sign Up !",
           });
+          return;
         }
       });
     } else {
