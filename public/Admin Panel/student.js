d3da758b51905ae59bf686ed67a412810d8f5354
KashanAdnan <khanzaidaboy@gmai.com>
2022-12-04 02:40:17 -0800

    some-code

--- public/Admin Panel/student.js
+++ public/Admin Panel/student.js
@@ -2,38 +2,38 @@
 // const Swal  = require("sweetalert2");
 // const app = express();
 
-var port = "https://sir-web.herokuapp.com";
-const signupdata = () => {
-  const Http = new XMLHttpRequest();
-  Http.open("GET", port + "/signupdata");
-  Http.setRequestHeader("Content-Type", "application/json");
-  Http.send(null);
-  var showdata = document.getElementById("showdata");
-  Http.onreadystatechange = (e) => {
-    console.log(e);
-    if (Http.readyState === 4) {
-      if (Http.status === 200) {
-        var jsonRes = JSON.parse(Http.responseText);
-        console.log(jsonRes)
-        var i = 1
-        let out;
-        jsonRes.map(data =>{
-            console.log(data)
-            out = `
-            <tr>
-            <td>${i++}</td>
-            <td>${data.username}</td>
-            <td>${data.email}</td>
-            <td>${data.phone}</td>
-            </tr>
+// var port = "https://sir-web.herokuapp.com";
+// const signupdata = () => {
+//   const Http = new XMLHttpRequest();
+//   Http.open("GET", port + "/signupdata");
+//   Http.setRequestHeader("Content-Type", "application/json");
+//   Http.send(null);
+//   var showdata = document.getElementById("showdata");
+//   Http.onreadystatechange = (e) => {
+//     console.log(e);
+//     if (Http.readyState === 4) {
+//       if (Http.status === 200) {
+//         var jsonRes = JSON.parse(Http.responseText);
+//         console.log(jsonRes)
+//         var i = 1
+//         let out;
+//         jsonRes.map(data =>{
+//             console.log(data)
+//             out = `
+//             <tr>
+//             <td>${i++}</td>
+//             <td>${data.username}</td>
+//             <td>${data.email}</td>
+//             <td>${data.phone}</td>
+//             </tr>
 
-            `
-            showdata.innerHTML += out
-        })
-      }
-    }
-  };
+//             `
+//             showdata.innerHTML += out
+//         })
+//       }
+//     }
+//   };
 
-  return false;
-};
-signupdata()
+//   return false;
+// };
+// signupdata()
